50b68547ad895803128519af71c31b9f14bfc791
Yaniv Gardi <ygardi@codeaurora.org>
2015-03-18 11:35:51 +0200

    fix building errors on Android
    3rd party HMAC SHA256 implementation has been added to the driver,
    and since driver common code (mmc_cmds.c) includes calling to
    routines and data structures that reside in these 3rd party files
    (under 3rdparty\hmac_sha) these files should be compiled as well
    in Android.mk makefile.
    
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>
    Signed-off-by: Chris Ball <chris@printf.net>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES:= mmc.c mmc_cmds.c
+LOCAL_SRC_FILES += 3rdparty/hmac_sha/sha2.c 3rdparty/hmac_sha/hmac_sha2.c
 LOCAL_MODULE := mmc_utils
 LOCAL_SHARED_LIBRARIES := libcutils libc
 LOCAL_C_INCLUDES+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
